0355358b25d3269b0967445a897ec36d98627cf6
Ethan Brooks <ethanabrooks@gmail.com>
2023-09-16 08:06:32 -0400

    rename to ad and ed
    fix explore_then_exploit

--- src/main.py
+++ src/main.py
@@ -74,7 +74,7 @@ def check_dirty():
     assert not Repo(".").is_dirty()
 
 
-parsers = dict(config=option("config", default="full_history"))
+parsers = dict(config=option("config", default="ad"))
 
 
 @tree.subcommand(parsers=dict(name=argument("name"), **parsers))
