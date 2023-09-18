659a82d5af4aedfe9449d8a9b691742dd7553734
ethanabrooks <ethanabrooks@gmail.com>
2023-09-18 08:37:40 -0400

    do some work on adpp

--- src/evaluators/adpp.py
+++ src/evaluators/adpp.py
@@ -1,7 +1,10 @@
+from dataclasses import asdict, dataclass
+
 import torch
 import torch.nn.functional as F
 
 import evaluators.ad
+from evaluators.ad import Step, clamp, get_dim
 
 
 class Evaluator(evaluators.ad.Evaluator):
@@ -10,15 +13,22 @@ class Evaluator(evaluators.ad.Evaluator):
         return Rollout(*args, **kwargs)
 
 
+@dataclass
 class PlanningRollout(evaluators.ad.Rollout):
+    first_observation: torch.Tensor
+    tasks: torch.Tensor
+    observations: torch.Tensor
+    actions: torch.Tensor
+    rewards: torch.Tensor
+
     def step(self, ctx: torch.Tensor) -> torch.Tensor:
-        A = evaluators.ad.get_dim(self.envs.action_space)
-        O = evaluators.ad.get_dim(self.envs.observation_space)
+        A = get_dim(self.envs.action_space)
+        O = get_dim(self.envs.observation_space)
         S = self.dataset.step_dim
         action = self.get_action(ctx)
         ctx[:, -A - 1 : -1] = action
         assert [*action.shape] == [self.n_rollouts, A]
-        action = evaluators.ad.clamp(action, self.envs.action_space)
+        action = clamp(action, self.envs.action_space)
         _, w = ctx.shape
         [reward] = self.predict(ctx, w - 2, w - 1).T
         assert [*reward.shape] == [self.n_rollouts]
@@ -28,12 +38,12 @@ class PlanningRollout(evaluators.ad.Rollout):
         observation = self.predict(ctx, -S, -S + O)
         assert [*observation.shape] == [self.n_rollouts, O]
         done = torch.zeros_like(reward, dtype=torch.bool)
-        return evaluators.ad.Step(
+        return Step(
             action=action, reward=reward, observation=observation, done=done, info=[]
         )
 
 
 class Rollout(evaluators.ad.Rollout):
     def get_action(self, ctx: torch.Tensor) -> torch.Tensor:
-        PlanningRollout.step
+        PlanningRollout(**asdict(self))
         raise NotImplementedError
